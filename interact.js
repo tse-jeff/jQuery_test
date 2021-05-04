54d5549d3c0d49ab371eddbed217cb9dd81cfe96
tse-jeff <jtse0316@gmail.com>
2021-05-03 23:55:16 -0400

    placed javascript code in js file

--- interact.js
+++ interact.js
@@ -4,3 +4,19 @@ $("body").prepend(
       <a href="interact.html">interactive</a>
     </div>`
     );
+
+var leftOffset = -500;
+
+var moveHeading = function () {
+  $("#heading").offset({left: leftOffset});
+
+  // speed
+  leftOffset += 2;
+
+  if (leftOffset > 2000) {
+    leftOffset = -500;
+  }
+}
+
+// calls moveHeading function every 1 millisecond
+setInterval(moveHeading, 1);
