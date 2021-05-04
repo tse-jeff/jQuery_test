cf74a446adfe5fd383fb4cfc42aadcd88f34ed8b
tse-jeff <jtse0316@gmail.com>
2021-05-04 00:10:20 -0400

    made text folow the mouse on the page

--- interact.js
+++ interact.js
@@ -1,14 +1,14 @@
 $("body").prepend(
-    `<div id="navbar">
+  `<div id="navbar">
       <a href="index.html">home</a>
       <a href="interact.html">interactive</a>
     </div>`
-    );
+);
 
 var leftOffset = -500;
 
 var moveHeading = function () {
-  $("#heading").offset({left: leftOffset});
+  $("#heading").offset({ left: leftOffset });
 
   // speed
   leftOffset += 2;
@@ -22,10 +22,20 @@ var moveHeading = function () {
 setInterval(moveHeading, 1);
 
 // responding to clicks
+// This simply prints to console when the text is clicked.
 function clickHandler(event) {
   console.log("Click! " + event.pageX + " " + event.pageY);
 }
 
 $("h1").click(clickHandler);
 
-// This simply prints to console when the text is clicked.
+// the mousemove event. 
+// this functions makes something follow the mouse
+$("html").mousemove(function (event) {
+  $("#mousemove").offset({
+    left: event.pageX,
+    top: event.pageY
+  });
+});
+
+
